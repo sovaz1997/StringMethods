43832cffe2ea5981877001a7e3f859f4bf101bc2
sovaz1997 <sovaz1997@yandex.ru>
2020-05-30 14:11:03 +0300

    init: first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const str = "Test string Test string";
+console.log(str.includes(" "));
+console.log(str.includes(" s"));
+console.log(str.includes(" ss"));
+
+console.log(str.includes("string"));
+console.log(str.indexOf("string"));
+console.log(str.indexOf("-"));
+
+console.log(str.indexOf("Test string"));
+console.log(str.lastIndexOf("Test string"));
