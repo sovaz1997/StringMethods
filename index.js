84b5934a8b0df057c409776e578bb5f7e0e0814e
sovaz1997 <sovaz1997@yandex.ru>
2020-05-30 14:22:02 +0300

    feat: test methods

--- index.js
+++ index.js
@@ -9,3 +9,27 @@ console.log(str.indexOf("-"));
 
 console.log(str.indexOf("Test string"));
 console.log(str.lastIndexOf("Test string"));
+
+console.log(str.replace("Test string", "Replaced"));
+console.log(str.split(' '));
+console.log(str.padStart(50, '?!'));
+console.log(str.padEnd(49, '?!'));
+
+console.log(str.charAt(7));
+console.log(str.repeat(5));
+
+const find = (str, word) => {
+    const start = str.indexOf(word);
+    const end = start + word.length;
+
+    if (start !== -1) {
+        return [
+            str.slice(0, start),
+            str.slice(start, end),
+            str.slice(end),
+
+        ];
+    }
+}
+
+console.log(find('Test string for split', 'g for'));
